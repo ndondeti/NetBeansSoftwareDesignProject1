116673e10e7bcb99ad513ca897db6c4b3f354a52
ndondeti <vdondeti@asu.edu>
2016-03-28 23:53:57 -0700

    Adding restful endpoints

--- CURD-GradeBook-vdondeti-NetBeans/src/main/java/edu/asu/cse564/curd/gradebook/RestEndPoint.java
+++ CURD-GradeBook-vdondeti-NetBeans/src/main/java/edu/asu/cse564/curd/gradebook/RestEndPoint.java
@@ -5,9 +5,6 @@
  */
 package edu.asu.cse564.curd.gradebook;
 
-import java.util.*;
-
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -23,6 +20,8 @@ import edu.asu.cse564.curd.gradebook.models.GradeBook;
 import edu.asu.cse564.curd.gradebook.models.GradeItem;
 import edu.asu.cse564.curd.gradebook.models.SetWorkItem;
 import edu.asu.cse564.curd.gradebook.models.WorkItem;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
 
 /**
  *
@@ -60,6 +59,34 @@ public class RestEndPoint {
         GradeItem gradeItem = gson.fromJson(jsonData, GradeItem.class);
         try{
             gradeBook.addGrade(studentName, gradeItem);
+            return Response.status(201).build(); 
+        }
+        catch(Exception e){
+            return Response.status(400).entity(e.getMessage()).build();
+        }
+    }
+    
+    @DELETE
+    @Path("removeGrade/{studentName}/{workItem}")
+    public Response removeGrade(@PathParam("studentName") String studentName,
+                                @PathParam("workItem")String workItem){
+        try{
+            gradeBook.deleteGrade(studentName, workItem);
+            return Response.status(201).build(); 
+        }
+        catch(Exception e){
+            return Response.status(400).entity(e.getMessage()).build();
+        }
+    }
+    
+    @PUT
+    @Path("updateGrade/{studentName}/{workItem}")
+    public Response removeGrade(@PathParam("studentName") String studentName,
+            @PathParam("workItem")String workItem, String updateMessage){
+        JsonObject jsonData = jsonBuilder.parse(updateMessage).getAsJsonObject();
+        GradeItem gradeItem = gson.fromJson(jsonData, GradeItem.class);
+        try{
+            gradeBook.updateGrade(studentName, gradeItem);
             return Response.status(201).build(); 
         }
         catch(Exception e){
